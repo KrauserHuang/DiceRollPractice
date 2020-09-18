b1892cc2b314b1e3aea6de993c11301dad23c0f5
krauserhuang <crescentdence@gmail.com>
2020-09-18 11:19:21 +0800

    Initial Commit

---
+++ DiceRollPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceRollPractice
+//
+//  Created by Tai Chin Huang on 2020/9/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
